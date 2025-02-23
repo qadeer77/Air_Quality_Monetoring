cf1bef25825c9d29618c6e680bb00670ac9655b6
Sameer Ahmed <balochtradersnayapay@gmail.com>
2025-02-23 12:59:47 +0500

    feat: add Firebase configuration and integrate environment variables for API keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,25 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; // Import Firestore
+
+// Import environment variables
+const apiKey = process.env.REACT_APP_FIREBASE_API_KEY;
+const authDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
+const projectId = process.env.REACT_APP_FIREBASE_PROJECT_ID;
+const storageBucket = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID;
+const appId = process.env.REACT_APP_FIREBASE_APP_ID;
+const measurementId = process.env.REACT_APP_FIREBASE_MEASUREMENT_ID;
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDt5Iy3TgW4hxcJ2KMblrafneBq71w8xnA",
-  authDomain: "air-quality-monitoring-sameer.firebaseapp.com",
-  projectId: "air-quality-monitoring-sameer",
-  storageBucket: "air-quality-monitoring-sameer.appspot.com",
-  messagingSenderId: "34430251763",
-  appId: "1:34430251763:web:fbea7b7da8f2ac45ee7d86",
-  measurementId: "G-772XKDBP3N",
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId,
 };
 
 // Initialize Firebase
@@ -17,3 +27,6 @@ const app = initializeApp(firebaseConfig);
 
 // Export Firebase Authentication
 export const auth = getAuth(app);
+
+// Export Firestore
+export const db = getFirestore(app);
